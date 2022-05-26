3a2ffcfa9abbeecb4fc53a88f3b0a55a02a61853
unknown <megyko1234@gmail.com>
2022-05-26 09:46:21 +0200

    Kész munka

--- src/calc.php
+++ src/calc.php
@@ -20,18 +20,35 @@ $magassag= $_GET['magassag'];
 
 
 
-
 function calcArea($tomeg,$magassag){
     
-    $t=$tomeg;
-    $area=$t/$magassag*$magassag;
+    $t=$tomeg/($magassag*$magassag);
+    $area=$t;
+
    
 
 
     return $area;
 }
 
-$page=str_replace('{{result}}', $area,$page);
+
+
+if(!empty($_GET['tomeg'])and
+!empty($_GET['magassag'])
+)
+{
+    $tomeg = $_GET['tomeg'];
+    $magassag = $_GET['magassag'];
+  
+    $area= calcArea($tomeg,$magassag);
+
+}else{
+    $area="Üres beviteli mező";
+}
+
+
+
+$page=str_replace('{{result}}', $area, $page);
 
 echo $page;
 
--- src/index.php
+++ src/index.php
@@ -10,6 +10,7 @@
 
 
 
+
 echo file_get_contents('templates/head.html');
 echo file_get_contents('templates/home.html');
 echo file_get_contents('templates/foot.html');
